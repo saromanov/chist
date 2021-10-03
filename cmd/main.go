c7a8f2f5cade26e8331ea1367a3c30c4a460506f
Sergey <xxsmotur@gmail.com>
2021-10-03 21:55:53 +0500

    Add argument parsing at the init of the app

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/saromanov/chist/pkg/chist"
+import (
+	"flag"
+	"github.com/saromanov/chist/pkg/chist"
+)
 
 var (
 	zshHistoryPath = ""
 )
 
+type config struct {
+	FilePath string 
+}
+
+func parse() config{
+	filePath := flag.String("path", "zsh", "path to zsh or bash history file")
+	flag.Parse()
+	return config{
+		FilePath: filePath,
+	}
+}
 func main() {
-	ch, err := chist.New(zshHistoryPath,10)
+	cfg := parse()
+	ch, err := chist.New(cfg.FilePath,10)
 	if err != nil {
 		panic(err)
 	}
